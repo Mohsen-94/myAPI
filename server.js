429afbbb194f5d0b27e19abb12faaf62c4e16a70
Mohsen-94 <34848846+Mohsen-94@users.noreply.github.com>
2019-01-16 11:01:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const { courseNames, validScheduels } = require('./app');
 
 app.get('/names', (req, res) => {
-  // res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(courseNames());
 });
 
 app.get('/courses', (req, res) => {
-  // res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', '*');
   let qString = req.query.string;
   // Send the json object
   res.send(validScheduels(qString));
